d2caf6766f2b9b6cb2056f9827c27a3f7258952a
Trevian <trevian2345@gmail.com>
2020-04-10 06:10:46 -0400

    Updated static collision tests for obstacle map (many simplifications made).  Added pdf for phases 3 and 4 (for reference).

--- obstacleMap.py
+++ obstacleMap.py
@@ -4,19 +4,28 @@ from math import *
 
 
 class ObstacleMap:
+    """
+    Class for obstacle map
+
+    Attributes
+    ----------
+    obstacles: list of np.array
+        List of obstacles.
+    """
     def __init__(self, distance):
         """
         Initialization of the obstacle map
-        :param distance: Minimum distance between the center of the robot from any point in the obstacle space.
-                         Accounts for both minimum clearance and the radius of the robot.
+        Parameters
+        ----------
+        distance: float
+            Minimum distance between the center of the robot from any point in the obstacle space.
+            Accounts for both minimum clearance and the radius of the robot.
         """
-        self.height = 200
-        self.width = 300
+        self.height = 10.0
+        self.width = 10.0
         self.thickness = distance
-        self.collisionMatrix = np.zeros((self.height, self.width), dtype=np.uint8)
         self.window_name = "Image"
         self.obstacles = []
-        self.obstacle_space = np.array([[]], dtype=np.uint8)
         self.create_obstacles()
 
     def show(self, image):
@@ -26,107 +35,108 @@ class ObstacleMap:
 
     def create_obstacles(self):
         """
-        Checks for collisions.
+        Defines obstacles on the map.
+        Each set of points is either an array of shape (2, 2) for the top-left and bottom-right corners of rectangles,
+        or an array of shape (3) for the (x, y, r) of a circle.
         """
-        # Define obstacles in the collision space.
-        # Upper-left polygon
-        points = [[25, 15],
-                  [75, 15],
-                  [20, 80]]
+        # Rectangles
+        points = np.array([[4.25, 0.25],
+                           [5.75, 1.75]], dtype=np.float64)
         self.obstacles.append(points)
-        points = [[75, 15],
-                  [100, 50],
-                  [75, 80],
-                  [50, 50]]
+        points = np.array([[2.25, 1.25],
+                           [2.75, 2.75]], dtype=np.float64)
         self.obstacles.append(points)
-        points = [[75, 15],
-                  [50, 50],
-                  [20, 80]]
+        points = np.array([[4.25, 8.25],
+                           [5.75, 9.75]], dtype=np.float64)
         self.obstacles.append(points)
 
-        # Lower-left rectangle
-        points = [[95, 170],
-                  [95 - 75*sqrt(3)/2, 170 - 75/2],
-                  [95 - 75*sqrt(3)/2 + 10/2, 170 - 75/2 - 10*sin(60*pi/180)],
-                  [95 + 10/2, 170 - 10*sin(60*pi/180)]]
+        # Circles
+        points = np.array([5.0, 5.0, 1.0], dtype=np.float64)
+        self.obstacles.append(points)
+        points = np.array([2.0, 7.0, 1.0], dtype=np.float64)
+        self.obstacles.append(points)
+        points = np.array([8.0, 3.0, 1.0], dtype=np.float64)
+        self.obstacles.append(points)
+        points = np.array([8.0, 7.0, 1.0], dtype=np.float64)
         self.obstacles.append(points)
 
-        # Lower-right rhombus
-        points = [[225, 160], [250, 175], [225, 190], [200, 175]]
-        self.obstacles.append(points)
-
-        # Upper-right circle
-        points = [[225, 50, 25, 25]]
-        self.obstacles.append(points)
-
-        # Middle ellipse
-        points = [[150, 100, 40, 20]]
-        self.obstacles.append(points)
-
-        # Create visualization
-        # self.obstacle_space = np.array([[int(self.is_colliding((row, col))) for col in range(self.width)]
-        #                                 for row in range(self.height)], dtype=np.uint8)
-        # self.show(self.obstacle_space)
-
-    def is_colliding(self, point, thickness=None, check_inside=True):
+    def collision_point(self, point):
         """
-        This function calculates the relative angle between the point and all the vertices of the image (between
-        -180 and 180 degrees).
-        If the sign of this direction ever changes, the point lies outside of the polygon.  Otherwise, the point
-        must lie outside of the polygon.
-        :param check_inside: Whether to check inside the polygon
-        :param thickness: distance to use for rigid robot.  Default uses radius and clearance.
-        :param point: The point to check for collisions
-        :return: True if there is a collision, False otherwise
+        This function checks whether a point lies within the obstacle space.
+
+        Returns
+        -------
+        bool
+            True if there is a collision, False otherwise
         """
-        t = self.thickness if thickness is None else thickness
-        ry = point[0]
-        rx = point[1]
-        if not (t <= ry <= self.height - t) or not (t <= rx <= self.width - t):
-            return True
-        for i in range(len(self.obstacles)):
-            # Polygons
-            if len(self.obstacles[i][0]) == 2:
-                direction = 0.0
-                collision = True
-                for j in range(len(self.obstacles[i]) + 1):
-                    # Check if the point is within range of any of the vertices
-                    p1 = self.obstacles[i][j % len(self.obstacles[i])]
-                    if ((rx - p1[0]) ** 2) + ((ry - p1[1]) ** 2) <= (t ** 2):
-                        return True
-                    # Check if the point is inside of the polygon
-                    elif not check_inside:
-                        collision = False
-                        continue
-                    vx, vy = self.obstacles[i][(j+1) % len(self.obstacles[i])]
-                    new_direction = atan2(vy - ry, vx - rx)
-                    new_direction = new_direction if new_direction >= 0.0 else (new_direction + 2.0 * pi)
-                    difference = new_direction - direction
-                    difference = difference if difference >= -pi else difference + 2.0 * pi
-                    difference = difference if difference <= pi else difference - 2.0 * pi
-                    direction = new_direction
-                    if j > 0 > difference:
-                        collision = False
-                if collision:
+        py, px = point
+        for obs in self.obstacles:
+            # Rectangle
+            if obs.shape == (2, 2):
+                y1, x1 = obs[0]
+                y2, x2 = obs[1]
+                if x1 <= px <= x2 and y1 <= py <= y2:
                     return True
-                else:
-                    # Check if the point is within a certain distance of an edge
-                    for j in range(len(self.obstacles[i])):
-                        x0, y0 = (rx, ry)
-                        x1, y1 = self.obstacles[i][j]
-                        x2, y2 = self.obstacles[i][(j + 1) % len(self.obstacles[i])]
-                        d = abs((y2 - y1)*x0 - (x2 - x1)*y0 + x2*y1 - y2*x1)/sqrt((y2-y1)**2 + (x2-x1)**2)
-                        if d < t:
-                            dot_product = (x0 - x1) * (x0 - x2) + (y0 - y1) * (y0 - y2)
-                            if dot_product < 0:
-                                return True
-            # Ellipse / circle
+            # Circle
             else:
-                vx, vy, vw, vh = self.obstacles[i][0]
-                vw += t
-                vh += t
-                if ((vx - rx) ** 2) + (((vy - ry) * vw/vh) ** 2) <= vw ** 2.0:
+                cy, cx, cr = obs[0]
+                if sqrt((cx - px) ** 2 + (cy - py) ** 2) <= cr:
                     return True
+        return False
+
+    def collision_circle(self, circle):
+        """
+        This function checks whether a circle lies within the obstacle space.
+
+        Parameters
+        ----------
+        circle: tuple of float
+            The circle to check for, in the form (x, y, r).
+
+        Returns
+        -------
+        bool
+            True if there is a collision, False otherwise.
+        """
+        cy, cx, cr = circle
+        # Check map boundaries
+        if not (cr <= cy <= self.height - cr) or not (cr <= cx <= self.width - cr):
+            return True
+
+        # Check obstacles
+        for obs in self.obstacles:
+            # Rectangles
+            if obs.shape == (2, 2):
+                # Check if the center of the circle lies within the rectangle
+                y1, x1 = obs[0]
+                y2, x2 = obs[1]
+                if y1 <= cy <= y2 and x1 <= cx <= x2:
+                    return True
+
+                corners = [(y1, x1), (y1, x2), (y2, x2), (y2, x1)]
+                for j in range(len(corners)):
+                    # Check if any of the vertices lies within the circle
+                    py1, px1 = corners[j]
+                    if sqrt((py1 - cy) ** 2 + (px1 - cx) ** 2) < cr:
+                        return True
+
+                    # Check if any of the edges of the rectangle intersects the circle
+                    py2, px2 = corners[(j + 1) % 4]
+                    if j % 2:
+                        if abs(px1 - cx) < cr and px1 <= cx <= px2:
+                            return True
+                    else:
+                        if abs(py1 - cy) < cr and py1 <= cy <= py2:
+                            return True
+
+            # Circles - check if centers are closer than the sum of the radii
+            else:
+                py, px, pr = obs[0]
+                if sqrt((px - cx) ** 2 + (py - cy) ** 2) < (pr + cr):
+                    return True
+        return False
+
+    def is_blocking(self):
         return False
 
 
